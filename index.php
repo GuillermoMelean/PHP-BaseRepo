4101883767d45af0a138e7a47857d8eac16d2459
Guillermo Alejandro Melean Ferreira <guial@live.com.pt>
2023-01-15 12:13:52 +0000

    Push Project

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1); 
+
+$request = $_SERVER['REQUEST_URI'];
+$arrayUrl = explode("/",trim($request));
+
+if(count($arrayUrl)>2){
+    $lang = strtoupper($arrayUrl[1]);
+    if(strlen($lang)>3){
+        $lang="pt";
+    }
+}else{
+    $lang = "pt";
+}
+
+$page = (count($arrayUrl)>2) ? $arrayUrl[2] : $arrayUrl[1]; 
+
+setcookie("lang", $lang, 0,'/');
+$_COOKIE["lang"] = $lang;
+
+switch (strtolower($page)) {
+
+    case 'index' :
+    case '' :
+    case '/';
+    case 'home';
+        require './views/index.php';
+        break;
+    case 'inicio-sessao' :
+    case 'login' :
+    case 'aceso' :
+    case 'connexion' :
+        require './views/login.php';
+        break;
+
+    case 'contactos':
+    case 'contacts':  
+        require './views/contacts.php';
+        break;
+
+    case 'propriedade' :
+    case 'property' :
+    case 'propiedad' :
+    case 'propriete' :
+        if($arrayUrl[3] != ""){
+            require './views/property.php';
+            break;
+        }
+    default:
+        require './views/404.php';
+        break; 
+}
---
+++ views/index.php
@@ -0,0 +1,7 @@
+<?php include_once("views/sections/section-header.php")?>
+<section id="content">
+	<div class="content-wrap"> 
+		<?php include_once("views/sections/section-homepage.php")?>
+	</div>
+</section>
+<?php include_once("views/sections/section-footer.php")?> 
